72dec4f18f9989d0383cdfdb4e70e2e4287ec29c
kennishida17 <ken.nishida@archibase.co.jp>
2023-04-28 14:44:26 +0900

    create form

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,10 @@ class Post(models.Model):
     intro= models.TextField()
     body = models.TextField()
     posted_date = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name="comments",on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    posted_date = models.DateField(auto_now_add=True)
